660bdcd6004b5434e2a048bcdb4e8ba4487324e4
Flo <Flo@10.1.98.250>
2016-11-28 23:52:48 +0100

    je suis de retour pour te efenrfkenr

--- src/java_projet/Java_projet.java
+++ src/java_projet/Java_projet.java
@@ -22,7 +22,7 @@ public class Java_projet extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'biatch'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
