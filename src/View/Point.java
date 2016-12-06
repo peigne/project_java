378c8bc02861e86274360580ab4188022f7a6181
Flo <Flo@10.1.98.250>
2016-12-06 17:09:17 +0100

    Classe point

---
+++ src/View/Point.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ *
+ * @author picaudfl
+ */
+public class Point implements IPoint {
+
+    private DoubleProperty x = new SimpleDoubleProperty();
+    private DoubleProperty y = new SimpleDoubleProperty();
+
+    public Point(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+
+    public Point(Point p) {
+        this(p.getX(), p.getY());
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "x=" + x + ", y=" + y + '}';
+    }
+
+    @Override
+    public DoubleProperty xProperty() {
+        return x;
+    }
+
+    @Override
+    public DoubleProperty yProperty() {
+        return y;
+    }
+
+    @Override
+    public Double getX() {
+        return x.get();
+    }
+
+    @Override
+    public Double getY() {
+        return y.get();
+    }
+
+    @Override
+    public void setX(double x) {
+        this.x.set(x);
+    }
+
+    @Override
+    public void setY(double y) {
+        this.y.set(y);
+    }
+
+    @Override
+    public void set(IPoint p) {
+        setX(p.getX());
+        setY(p.getY());        
+    }
+
+}
+
