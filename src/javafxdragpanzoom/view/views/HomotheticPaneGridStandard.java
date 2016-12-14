ed499a668899c0c5cd5d22b9021d470acf1d8889
anas darwich <anas.darwich@aliceadsl.fr>
2016-12-14 19:03:29 +0100

    mis a jour de la fonction zoom: probleme de synchronisation toujours pas resolu, des ameliorations en vue

--- src/javafxdragpanzoom/view/views/HomotheticPaneGridStandard.java
+++ src/javafxdragpanzoom/view/views/HomotheticPaneGridStandard.java
@@ -1,45 +1,71 @@
 package javafxdragpanzoom.view.views;
+
 import java.awt.Point;
+import javafx.geometry.Point2D;
+import javafx.scene.transform.Affine;
 
 /**
- * Pane homothétique dont la mise à l'échelle est gérée par les méthodes standard de Node
+ * Pane homothétique dont la mise à l'échelle est gérée par les méthodes
+ * standard de Node
+ *
  * @author saporito
  */
 public class HomotheticPaneGridStandard extends AbstractHomotheticPaneGrid {
+
+    private Affine transfo;
 
     public HomotheticPaneGridStandard() {
         super();
         scaleXProperty().bind(scaleProperty());
         scaleYProperty().bind(scaleProperty());
-        
+        transfo = new Affine();
+        this.getTransforms().add(transfo);
+
     }
 
     @Override
     public void setScale(double scale) {
         scaleProperty().setValue(scale);
-        
+
     }
 
     @Override
     public void setScale(double scale, double pivotX, double pivotY) {
-        Point point2D= new Point();
+        /*Point point2D= new Point();
         point2D.setLocation(localToScreen(pivotX,pivotY).getX(),localToScreen(pivotX,pivotY).getY());
         scaleProperty().setValue(scale);        
-        translate(point2D.getX()-localToScreen(pivotX,pivotY).getX(),point2D.getY()-localToScreen(pivotX,pivotY).getY());
-        
-                
+        translate(point2D.getX()-localToScreen(pivotX,pivotY).getX(),point2D.getY()-localToScreen(pivotX,pivotY).getY());*/
+        transfo.appendScale(scale, scale, pivotX, pivotY);
+        System.err.println((transfo));
 
     }
 
     @Override
     public void addScale(double deltaScale, double pivotX, double pivotY) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    
+        if (deltaScale>0)
+                {   
+                    scaleProperty().setValue(getScale()*1.1);
+                    System.err.println("scale"+getScale());
+                    this.setScale(getScale(),pivotX,pivotY);
+
+                }
+                else 
+                {
+                    scaleProperty().setValue(scaleProperty().getValue()*(1/1.1));
+                    System.err.println("scale"+scaleProperty().getValue());
+                    this.setScale(this.getScale(),pivotX,pivotY);
+                    
+                }    }
 
     @Override
     public void translate(double dx, double dy) {
-        this.setLayoutX(getLayoutX()+dx);
-        this.setLayoutY(getLayoutY()+dy);
-          }
-    
+        /*Affine transfo_translate= new Affine();
+        transfo_translate.appendTranslation(this.getLayoutX()+dx, this.getLayoutY()+dy);
+        this.getTransforms().set(0, transfo_translate);
+        System.err.println("ok---");*/
+        this.setLayoutX(this.getLayoutX() + dx);
+        this.setLayoutY(this.getLayoutY() + dy);
+    }
+
 }
