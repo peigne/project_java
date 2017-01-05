22e5dfdff62748dca600692cc4b815d2217968a9
flori <flori@LAPTOP-6SBMJBBP>
2017-01-05 22:03:51 +0100

    toto

--- src/View/View/View.java
+++ src/View/View/View.java
@@ -21,24 +21,16 @@ import javafx.scene.paint.Paint;
 import View.View.Radar;
 
 public final class View extends Control{
-    private static Dimension SCREEN_DIMENSIONS = Toolkit.getDefaultToolkit().getScreenSize();
-    public static double SCREEN_HEIGHT = SCREEN_DIMENSIONS.getHeight();
-    public static double SCREEN_WIDTH = SCREEN_DIMENSIONS.getWidth();
     public static visualparameters.VisualParameters param = visualparameters.VisualParametersManager.load();
 
     public View() {
-        this.setBackground(new Background(new BackgroundFill[]{new BackgroundFill(param.getSceneBackgroundColor(), new CornerRadii(0.0), new Insets(0.0))}));
+        this.setBackground(new Background(new BackgroundFill[]{new BackgroundFill(param.getSceneBackgroundColor(), new CornerRadii(0), new Insets(0))}));
         Pane pane = new Pane();
         this.getChildren().add(pane);
         Radar radar = new Radar();
         //SectorView sector=new SectorView();
         pane.getChildren().add(radar);
-        //MenusManager.createAndManageGeneralMenu(pane);
-        //MenusManager.createAndManageGeneralContextMenu(this);
-        //radar.translate(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0);
-        //double dsH = SCREEN_WIDTH / radarViewPane.getBoundsInLocal().getWidth();
-        //double dsV = SCREEN_HEIGHT / radarViewPane.getBoundsInLocal().getHeight();
-        //radarViewPane.addScale(Math.min(dsH, dsV), 0.0, 0.0);
+
     }
 }
 
